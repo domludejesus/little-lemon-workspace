15de286c2502499f608be3a0329e6b085ff5f897
domludejesus <domludejesus@gmail.com>
2023-11-07 16:38:07 -0500

    'setting up static routes'

--- restaurant/views.py
+++ restaurant/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-def sayHello(request):
- return HttpResponse('Welcome to Little Lemon')
+def index(request):
+    return render(request, 'index.html', {})
