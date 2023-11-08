6b23482104b7bcdc3b0f3cb8144ae77212bc361b
domludejesus <domludejesus@gmail.com>
2023-11-08 10:15:36 -0500

    ' two classes menu item and single menu item'

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+# Create your views here.
+from rest_framework.generics import ListCreateAPIView
+from .models import Menu
+from .serializers import menuSerializer
+from rest_framework.generics import RetrieveUpdateAPIView, DestroyAPIView
 
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = menuSerializer
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = menuSerializer
